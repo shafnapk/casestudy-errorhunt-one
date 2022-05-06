3e34809735422e9576a381d823b37d2ccea4884e
shafnapk <66784606+shafnapk@users.noreply.github.com>
2022-05-06 22:17:40 +0400

    Remaining files commit

---
+++ src/routes/loginroute.js
@@ -0,0 +1,38 @@
+const express = require('express'); 
+const loginRouter = express.Router();
+const user = require('../data/user');
+
+loginRouter.get('/',function(req,res){
+
+    res.render('login',{});
+    
+})
+
+
+loginRouter.post('/check',function(req,res){
+    // Fixed param to params
+    var checkuser = req.body;    
+    console.log(checkuser);
+    var flag=false;
+    for(let i=0;i<user.length;i++){    
+        if(checkuser.uid==user[i].uid && checkuser.pwd==user[i].pwd){            
+            flag=true;
+            break;
+        }
+        else{
+                flag=false;
+        }
+    };
+    console.log(flag);
+    let result = toString(flag)
+    console.log(result)
+    res.end(result)
+/* 
+ */
+
+});
+
+
+
+
+module.exports = loginRouter;
