59a9781903c96766502d737105888acb730406ae
unknown <pkshafna123@gmail.com>
2022-07-01 22:25:25 +0530

    added mongo db external

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,19 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+
+const mongoAtlasUri = 'mongodb+srv://pkshafna:123@cluster0.vzhbl.mongodb.net/?retryWrites=true&w=majority';
+ try {
+    // Connect to the MongoDB cluster
+    mongoose.connect(
+      mongoAtlasUri,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected"),
+    );
+  } catch (e) {
+      console.log(e);
+    console.log("could not connect");
+  }
+
+//mongoose.connect('mongodb://localhost:27017/Library');
 const Schema = mongoose.Schema;
 
 
